81347cdb24207ea9a687ec8c2705a8b7c32a52dd
Liu Ren <liuren@cc1st.com>
2018-10-13 20:41:45 +0800

    实现多用户博客系统

--- app.js
+++ app.js
@@ -1,42 +1,69 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const logger = require('koa-logger')
 const serve = require('koa-static');
+const bodyParser = require('koa-bodyparser');
 const views = require('koa-views');
+const mongoose = require('mongoose');
 
 const app = new Koa();
 const router = new Router();
 
+app.use(logger()).use(bodyParser())
 app.use(views(__dirname + '/views', {extension: 'pug'}));
+app.use(serve(__dirname + 'bower_components'));
+app.use(serve(__dirname + '/public'));
 
 const ip = '127.0.0.1';
 const post = '3000';
 
-router.get('/', async (ctx) => {
-  let articles = [
-    {
-      id: 1,
-      title: 'Title One',
-      author: 'hfpp2012'
-    },
-    {
-      id: 2,
-      title: 'Title Two',
-      author: 'hfpp2012'
-    },
-    {
-      id: 3,
-      title: 'Title Three',
-      author: 'hfpp2012'
-    }
-  ]
-  await ctx.render('index',{
-    title: 'article-blog-protal',
-    articles
-  })
+mongoose.connect(`mongodb://${ip}/article-blog`, {useNewUrlParser: true});
+
+let db = mongoose.connection;
+
+const Article = require('./models/article');
+
+let articles = [];
+Article.find({}, (err, value) => {
+  articles = value
 });
+
+router.get('/', async (ctx) => {
+  await ctx.render('index', {
+    title: 'article-blog-protal',
+    articles,
+  });
+})
+
+router.get('/articles/new', async (ctx) => {
+  await ctx.render('new', {
+    title: 'Add Article'
+  });
+})
+
+router.post('/articles/create', async (ctx) => {
+  let article = new Article(ctx.request.body);
+  await article.save(async (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(ctx.redirect);
+      await ctx.redirect('/')
+    }
+  })
+})
 
 app.use(router.routes());
 
-app.listen(post, () => {
-  console.log(`Server is listening on http://${ip}:${post}`);
-})
+db.once('open', () => {
+  console.log('Connected to Mongodb');
+  app.listen(post, () => {
+    console.log(`Server is listening on http://${ip}:${post}`);
+  })
+});
+
+db.on('error', (err) => {
+  console.log(err);
+});
+
+
